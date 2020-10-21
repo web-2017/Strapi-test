22a31da6d9e984de4c6062b14d139d684afc4b69
Mario <webdevelope2017@gmail.com>
2020-10-21 10:53:10 +0300

    Plugin Folders and Files Architecture

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,8 +1,17 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  find: async (ctx) => {
+    // console.log(ctx.request.body);
+    console.log(ctx);
+    const dd = await strapi.
+    const article = await strapi.query("article").find();
+    console.log(article);
+    return article;
+  },
+};
