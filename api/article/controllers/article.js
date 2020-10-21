56b769f3841bd2c3824200314c2201c6de8ed650
Mario <webdevelope2017@gmail.com>
2020-10-21 15:13:07 +0300

    wysiwyg plugin

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -8,8 +8,6 @@
 module.exports = {
   find: async (ctx) => {
     // console.log(ctx.request.body);
-    console.log(ctx);
-    const dd = await strapi.
     const article = await strapi.query("article").find();
     console.log(article);
     return article;
