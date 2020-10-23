ce5e7d2849d86b1e2bc58925ca66e322a62bad6b
Mario <webdevelope2017@gmail.com>
2020-10-23 15:37:02 +0300

    mend

--- plugins/import-content/controllers/import-content.js
+++ plugins/import-content/controllers/import-content.js
@@ -25,7 +25,7 @@ module.exports = {
   preAnalyzeImportFile: async (ctx) => {
     const services = strapi.plugins["import-content"].services;
     try {
-      const data = await services["importcontent"].preAnalyzeImportFile(ctx);
+      const data = await services["import-content"].preAnalyzeImportFile(ctx);
       ctx.send(data);
     } catch (error) {
       console.log(error);
